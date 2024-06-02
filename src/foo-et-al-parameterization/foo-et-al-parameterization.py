03b205d6d6b10cb4558ced4ea6e48ce37761f254
aidangold <goldstai@oregonstate.edu>
2024-06-02 14:47:54 -0400

    build files and source files

--- src/foo-et-al-parameterization/foo-et-al-parameterization.py
+++ src/foo-et-al-parameterization/foo-et-al-parameterization.py
@@ -5,8 +5,6 @@ import math
 
 def sphere_volume(radius):
     """ sphere_volume takes a given numerical value radius as a parameter of a sphere
-    and returnsback the total volume of the sphere. """
+    and returns the total volume of the sphere. """
 
-    volume = 4.0 / 3.0 * math.pi * (radius ** 3)
-    return volume
-
+    return 4.0 / 3.0 * math.pi * (radius ** 3)
